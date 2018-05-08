5574cfcd7dd54367856ca93b3a1c148816a2806d
原嶋優衣 <yui1993@live.jp>
2018-05-08 22:30:54 +0900

    ScriptTest
    再々提出です。

---
+++ Assets/Kadai.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kadai : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int[] array = new int[5];
+
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
+        array[3] = 40;
+        array[4] = 50;
+
+        for (int i = 0; i < 5; i++)
+            Debug.Log(array[i]);
+        for (int x = 4; 0 <= x; x--)
+            Debug.Log(array[x]);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    private int hp = 100;     //体力
+    private int power = 25;  //攻撃力
+    private int mp = 53;    //魔法攻撃
+
+    //攻撃用の関数
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    //防御用の関数
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        //残りhpを減らす
+        this.hp -= damage;
+    }
+
+    //魔法攻撃の関数
+    public void Magic(){
+        int mpoint = 0;
+        mpoint += 5;
+        if (mp >= mpoint)
+        {
+            Debug.Log("魔法攻撃をした。残りMPは" + (mp - mpoint));
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない");
+        }
+        //残りmpを減らす
+        this.mp -= mpoint;
+    }
+}
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        //Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+
+        //攻撃用の関数を呼び出す
+        lastboss.Attack();
+        //防御用の関数を呼び出す
+        lastboss.Defence(3);
+        //魔法攻撃の関数を呼び出す
+        for (int i = 0; i <= 10; i++)
+        {
+            lastboss.Magic();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
